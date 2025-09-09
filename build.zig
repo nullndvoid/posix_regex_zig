7a7f875055012a4e49822cfcf05d54522392b24c
Jacob <jjh413@student.bham.ac.uk>
2025-09-09 18:42:46 +0100

    Fix module name

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("regex_posix", .{
+    const mod = b.addModule("posix_regex", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
